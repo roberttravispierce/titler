a90ab5a7e97e6bf6a28530ce2884d83ab2e2fdd5
Robert Travis Pierce <robert@roberttravispierce.com>
2017-07-14 21:38:04 -0500

    Add i18n locale generator

--- lib/generators/titler/install/install_generator.rb
+++ lib/generators/titler/install/install_generator.rb
@@ -4,12 +4,16 @@ module Titler
   module Generators
     class InstallGenerator < Rails::Generators::Base
       source_root File.expand_path('../templates', __FILE__)
-      desc "Creates Titler initializer for your application"
+      desc "Creates Titler initializer and i18n for your application"
 
       def copy_initializer
         template 'titler.rb', 'config/initializers/titler.rb'
 
         puts "Install complete!"
+      end
+
+      def copy_i18n
+        template 'titler.en.yml', 'config/locales/titler.en.yml'
       end
     end
   end

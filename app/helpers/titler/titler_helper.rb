8fb4f8e3c29fc635b5434b28487f0345223defe8
Robert Travis Pierce <roberttravispierce@gmail.com>
2019-10-22 18:30:05 -0500

    Fix all but one rubocop problem

--- app/helpers/titler/titler_helper.rb
+++ app/helpers/titler/titler_helper.rb
@@ -1,7 +1,11 @@
+# frozen_string_literal: true
+
+# Main module for the gem
 module Titler
+  # Helper module for the gem
   module TitlerHelper
     def page_title(page_title)
-      content_for(:page_title) {page_title}
+      content_for(:page_title) { page_title }
     end
 
     def titler(custom_vars = {})
@@ -14,6 +18,7 @@ module Titler
       ).to_s
     end
 
+    # Main class for the gem
     class Title
       def initialize(controller:, i18n:, title_as_set:, context:)
         @controller = controller
@@ -24,20 +29,20 @@ module Titler
       end
 
       def to_s
-        th = title_hash = Hash.new
-        th[:env_prefix] = env_prefix
-        th[:admin_namespace] = admin_namespace
-        th[:title_body] = title_body
-        th[:app_name] = app_name
-        th[:app_tagline] = app_tagline
-        build_title_str(th)
+        titler = {}
+        titler[:env_prefix] = env_prefix
+        titler[:admin_namespace] = admin_namespace
+        titler[:title_body] = title_body
+        titler[:app_name] = app_name
+        titler[:app_tagline] = app_tagline
+        build_title_str(titler)
       end
 
       private
 
       def env_prefix
         if @configuration.use_env_prefix
-          Rails.env.production? ? '' : "(#{Rails.env[0,1].upcase}) "
+          Rails.env.production? ? '' : "(#{Rails.env[0, 1].upcase}) "
         else
           ''
         end
@@ -56,7 +61,8 @@ module Titler
       end
 
       def delimiter
-        if @i18n.exists?('titler.delimiter') && @i18n.t('titler.delimiter').present?
+        if @i18n.exists?('titler.delimiter') &&
+           @i18n.t('titler.delimiter').present?
           @i18n.t('titler.delimiter')
         else
           @configuration.delimiter
@@ -64,63 +70,85 @@ module Titler
       end
 
       def app_name
-        if @i18n.exists?('titler.app_name') && @i18n.t('titler.app_name').present?
+        if @i18n.exists?('titler.app_name') &&
+           @i18n.t('titler.app_name').present?
           @i18n.t('titler.app_name')
         else
-          Rails.application.class.to_s.split("::").first
+          Rails.application.class.to_s.split('::').first
         end
       end
 
       def app_tagline
-        tagline = @i18n.exists?('titler.app_tagline') ? @i18n.t('titler.app_tagline') : ''
+        @i18n.exists?('titler.app_tagline') ? @i18n.t('titler.app_tagline') : ''
       end
 
       def title_body
         if @title_as_set.present?
           @title_as_set
         else
-          @controller.controller_name.titleize + ' ' + @controller.action_name.titleize rescue ''
+          "#{controller_str} #{action_name_str}"
         end
       end
 
-      def build_title_str(th)
-        case @configuration.app_name_position
-        when 'append'
-          th[:env_prefix] + th[:admin_namespace] + th[:title_body] + app_tagline_str(th) + app_name_str(th)
-        when 'prepend'
-          th[:env_prefix] + app_name_str(th) + app_tagline_str(th) + th[:admin_namespace] + th[:title_body]
+      def build_title_str(titler)
+        if @configuration.app_name_position
+          title_str_options(titler)[@configuration.app_name_position]
         else
-          th[:env_prefix] + th[:admin_namespace] + th[:title_body] + app_tagline_str(th)
+          titler[:env_prefix] +
+            titler[:admin_namespace] +
+            titler[:title_body] +
+            app_tagline_str(titler)
         end
       end
 
-      def app_name_str(th)
+      def title_str_options(titler) # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
+        { 'append' => titler[:env_prefix] +
+          titler[:admin_namespace] +
+          titler[:title_body] +
+          app_tagline_str(titler) +
+          app_name_str(titler),
+          'prepend' => titler[:env_prefix] +
+            app_name_str(titler) +
+            app_tagline_str(titler) +
+            titler[:admin_namespace] +
+            titler[:title_body],
+          '' => '' }.freeze
+      end
+
+      def app_name_str(titler)
         if title_body.blank?
-          th[:app_name]
+          titler[:app_name]
         else
-          case @configuration.app_name_position
-          when 'append'
-            delimiter + th[:app_name]
-          when 'prepend'
-            th[:app_name] + delimiter
-          else
-            ''
-          end
+          app_name_position_options(titler)[@configuration.app_name_position]
         end
       end
 
-      def app_tagline_str(th)
-        tagline = th[:app_tagline]
+      def app_name_position_options(titler)
+        { 'append' => delimiter + titler[:app_name],
+          'prepend' => titler[:app_name] + delimiter,
+          '' => '' }.freeze
+      end
+
+      def app_tagline_str(titler)
+        tagline = titler[:app_tagline]
         if tagline.blank? || !@configuration.use_app_tagline
           ''
         else
-          case @configuration.app_name_position
-          when 'append'
-            delimiter + tagline
-          when 'prepend'
-            tagline + delimiter
-          end
+          app_tagline_options(tagline)[@configuration.app_name_position]
         end
+      end
+
+      def app_tagline_options(tagline)
+        { 'append' => delimiter + tagline,
+          'prepend' => tagline + delimiter }.freeze
+      end
+
+      def controller_str
+        @controller.controller_name.titleize
+      end
+
+      def action_name_str
+        @controller.action_name.titleize
       end
     end
   end
